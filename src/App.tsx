44d3a963dc5fd2742d36d17a96fec3fcab8ae4b1
Akylbek <hsplay74@gmail.com>
2024-10-26 09:36:09 +0600

    app v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-
+import Home from "./containers/Home/Home.tsx";
+import ToolBar from "./components/ToolBar/ToolBar.tsx";
+import {Route, Routes} from "react-router-dom";
+import NewPost from "./containers/NewPost/NewPost.tsx";
+import PostItem from "./containers/PostItem/PostItem.tsx";
 
 const App = () => {
     return (
         <>
-
+            <header>
+                <ToolBar />
+            </header>
+            <Routes >
+                <Route path="/" element={<Home />} />
+                <Route path="/posts/add" element={<NewPost />} />
+                <Route path="/posts/:id" element={<PostItem />} />
+                <Route path="/posts/:id/edit" element={<NewPost />} />
+            </Routes>
         </>
     );
 };
 
-export default App
+export default App;
